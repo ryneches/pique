01e1b8d2e876018f1aa174bfb941458be978dc7f
Russell Neches <git@vort.org>
2011-07-12 20:30:12 -0700

    added Tk based GUI

--- pique.py
+++ pique.py
@@ -4,30 +4,152 @@ Pique.
 """
 import pique
 import sys
+from Tkinter import *
+from tkFileDialog import *
 
-IP_file = sys.argv[1]
-BG_file = sys.argv[2]
+# create the main window
+root = Tk()
 
-if len(sys.argv) > 3 :
-    map_file = sys.argv[3]
-    D = pique.data.PiqueData( IP_file, BG_file, map_file=map_file )
-else :
-    D = pique.data.PiqueData( IP_file, BG_file )
+class PiqueApp :
+    def __init__( self, master ) :
+        self.IPfile  = None
+        self.BGfile  = None
+        self.mapfile = None
+        self.alpha   = 300
+        self.l_thresh= 30
+        self.master = master 
 
-# FIXME : THIS IS BECAUSE OUR TEST DATA HAS ERRORS IN IT.
-del(D.data['PNRC100'])
+        master.title( 'Pique 1.0' )
+        
+        # IP file 
+        frame1 = Frame( master )
+        self.IPfilebutton = Button( frame1,                 \
+                                    text='IP Track',        \
+                                    command=self.setIPfile  )
+        self.IPfilebutton.pack( side=RIGHT, fill=X )
+        
+        self.IPfiletext  = Entry(   frame1                  )
+        self.IPfiletext.pack( side=LEFT )
+        frame1.pack( fill=X )
+         
+        # BG file 
+        frame2 = Frame( master )
+        self.BGfilebutton = Button( frame2,                 \
+                                    text='BG Track',        \
+                                    command=self.setBGfile  )
+        self.BGfilebutton.pack( side=RIGHT, fill=X )
+        
+        self.BGfiletext  = Entry(   frame2                  )
+        self.BGfiletext.pack( side=LEFT )
+        frame2.pack( fill=X )
+        
+        # map file
+        frame3 = Frame( master )
+        self.mapfilebutton = Button(frame3,                 \
+                                    text='Map (optional)',  \
+                                    command=self.setmapfile )
+        self.mapfilebutton.pack( side=RIGHT, fill=X )
+        
+        self.mapfiletext  = Entry(  frame3                  )
+        self.mapfiletext.pack( side=LEFT )
+        frame3.pack( fill=X )
+        
+        # alpha
+        frame4 = Frame( master )
+        self.alphatext    = Entry(  frame4                  )
+        self.alphatext.pack( side=LEFT )
+        self.alphatext.insert( 0, self.alpha )        
+        
+        self.alphalabel = Label( frame4, text='Fragment length' )
+        self.alphalabel.pack( side=RIGHT )
+        frame4.pack( fill=X )       
+        
+        # l_thresh
+        frame5 = Frame( master )
+        self.lthreshtext  = Entry(  frame5                  )
+        self.lthreshtext.pack( side=LEFT )
+        self.lthreshtext.insert( 0, self.l_thresh )        
+        
+        self.lthreshlabel = Label( frame5, text='Read length' )
+        self.lthreshlabel.pack( side=RIGHT )
+        frame5.pack( fill=X )
+        
+        # control buttons
+        frame6 = Frame( master )
+        self.runbutton    = Button( frame6,                 \
+                                    text='Run',             \
+                                    command=self.run        )
+        self.runbutton.pack( side=RIGHT )
+        
+        self.quitbutton   = Button( frame6,                 \
+                                    text='Quit',            \
+                                    command=master.quit     )
+        self.quitbutton.pack( side=LEFT )
+        frame6.pack( fill=X )
+        
+    def setIPfile( self ) :
+        self.IPfile = askopenfilename()
+        l = len(self.IPfiletext.get())
+        self.IPfiletext.delete( 0, l )
+        self.IPfiletext.insert( 0, self.IPfile )
+        print 'IP file : ' + self.IPfile
+        
+    def setBGfile( self ) :
+        self.BGfile = askopenfilename()
+        l = len(self.BGfiletext.get())
+        self.BGfiletext.delete( 0, l )
+        self.BGfiletext.insert( 0, self.BGfile )
+        print 'BG file : ' + self.BGfile
+        
+    def setmapfile( self ) :
+        self.mapfile = askopenfilename()
+        l = len(self.mapfiletext.get())
+        self.mapfiletext.delete( 0, l )
+        self.mapfiletext.insert( 0, self.mapfile )
+        print 'map file : ' + self.mapfile
+        
+    def run( self ) :
+        
+        # check inputs...
+        alpha    = int( self.alphatext.get().strip() )
+        l_thresh = int( self.lthreshtext.get().strip() )
+        
+        # load the data
+        pique.msg( 'loading data...' )
+        self.master.title( 'Pique : loading data...' )
+        if not self.mapfile :
+            D = pique.data.PiqueData( self.IPfile, self.BGfile )
+        else :
+            D = pique.data.PiqueData( self.IPfile, self.BGfile, self.mapfile )     
+        
+        # start analysis workbench
+        pique.msg( 'creating analysis workbench...' )
+        self.master.title( 'Pique : creating workbench...' )
+        PA = pique.analysis.PiqueAnalysis( D )
+        
+        # run filters
+        pique.msg( 'running filters...' )
+        self.master.title( 'Pique : running filters...' )
+        pique.msg( '  -> alpha    : ' + str(alpha) )        
+        pique.msg( '  -> l_thresh : ' + str(l_thresh) )
+        PA.filter_all( alpha, l_thresh )
+        
+        # find peaks
+        pique.msg( 'finding peaks...' )
+        self.master.title( 'Pique : finding peaks...' )
+        for ar_name in PA.data.keys() :
+            PA.find_peaks(ar_name)
+        
+        # write output files
+        pique.msg( 'writing output files...' )
+        self.master.title( 'Pique : writing output...' )
+        pique.fileIO.writepeaksGFF( 'test_out.gff', PA.data )
+        pique.fileIO.writebookmarks( 'test.bookmark', PA.data )
+        
+        # done!
+        pique.msg( 'run completed.' )
+        self.master.title( 'Pique : run completed.' )
 
-pique.msg( 'loading data...' )
-PA = pique.analysis.PiqueAnalysis( D )
+app = PiqueApp( root )
 
-pique.msg( 'filtering data...' )
-PA.filter_all( 300, 30 )
-
-pique.msg( 'finding peaks...' )
-for ar_name in PA.data.keys() :
-    PA.find_peaks(ar_name)
-
-pique.msg( 'writing output files...' )
-pique.fileIO.writepeaksGFF( 'test_out.gff', PA.data )
-pique.fileIO.writebookmarks( 'test.bookmark', PA.data )
-
+root.mainloop()
