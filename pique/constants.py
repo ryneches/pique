bb0f98a5f8f914b9d4e30c161fc7c7f987b94f03
Russell Neches <git@vort.org>
2013-01-01 01:30:36 -0800

    cut a new release; now with bam2wav

--- pique/constants.py
+++ pique/constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Pique version
-version         = '0.1.4'
+version         = '0.1.5'
 
 # cache directory for storing temporary and downloaded files
 cache_dir       = '/tmp/pique'
